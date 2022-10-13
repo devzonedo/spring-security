1839f1fb7aa8d3f9467673614b18c1f2de29fe74
Ravinath Fernando <devzonedo@gmail.com>
2022-10-13 22:11:38 +0530

    lesson 1 basic authentication

---
+++ lesson-1/ss-cp/src/main/java/com/devz/sscp/SsCpApplication.java
@@ -0,0 +1,13 @@
+package com.devz.sscp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsCpApplication.class, args);
+    }
+
+}
---
+++ lesson-1/ss-cp/src/test/java/com/devz/sscp/SsCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devz.sscp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsCpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
